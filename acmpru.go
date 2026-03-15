2b4c91f40f1e66d0458e77f49767c1df1b3436b5
Lorriant <brearey4@gmail.com>
2026-03-15 18:35:21 +0900

    acmpru file

---
+++ acmpru.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func Acmp1() {
+	var a int32
+	var b int32
+
+	fmt.Scanf("%d %d", &a, &b)
+	fmt.Println(a + b)
+}
+
+func Acmp2() {
+	// 5 -> 15
+	var N int
+	fmt.Scan(&N)
+	var sum int
+
+	if (N > 0) {
+		for i := 1; i <= N; i++ {
+			sum += i
+		}
+		fmt.Println(sum)
+	} else {
+		for i := 1; i >= N; i-- {
+			sum += i
+		}
+		fmt.Println(sum)
+	}
+}
--- main.go
+++ main.go
@@ -1,36 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
-func acmp1() {
-	var a int32
-	var b int32
-
-	fmt.Scanf("%d %d", &a, &b)
-	fmt.Println(a + b)
-}
-
-func acmp2() {
-	// 5 -> 15
-	var N int
-	fmt.Scan(&N)
-	var sum int
-
-	if (N > 0) {
-		for i := 1; i <= N; i++ {
-			sum += i
-		}
-		fmt.Println(sum)
-	} else {
-		for i := 1; i >= N; i-- {
-			sum += i
-		}
-		fmt.Println(sum)
-	}
-}
-
 func main() {
-	acmp2()
+	Acmp2()
 }
